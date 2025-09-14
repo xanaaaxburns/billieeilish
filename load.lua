9780941f54c88aaa7fc2c2d560e0d9e75235cc3a
xanaaaxburns <alphxx.jd@gmail.com>
2025-09-14 03:39:03 +0100

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,22 @@
+local placeId = game.PlaceId
+
+if placeId == 2788229376 then
+    loadstring("https://raw.githubusercontent.com/xanaaaxburns/billieeilish/refs/heads/main/dhsczx.lua")()
+else
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Infinite Yield",
+        Text = "Do you want to run Infinite Yield?",
+        Duration = 10,
+        Button1 = "Yes",
+        Button2 = "No",
+        Icon = "rbxassetid://126864685454351",
+        Callback = function(button)
+            if button == "Yes" then
+                loadstring(game:HttpGet('https://raw.githubusercontent.com/xanaaaxburns/billieeilish/refs/heads/main/bilyield'))()
+            else
+                script:Destroy()
+            end
+        end
+    })
+end
