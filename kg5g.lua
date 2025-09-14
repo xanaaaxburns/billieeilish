359c083e8f48c60b1c386a1caa96e246b565dd57
xanaaaxburns <alphxx.jd@gmail.com>
2025-09-14 03:36:52 +0100

    Create kg5g.lua

---
+++ kg5g.lua
@@ -0,0 +1,4 @@
+getgenv()._ =
+    'Join discord.gg/msgabv2t9Q | discord.gg/stando to get latest update. | If you pay for this script you get scammed, this script is completely free ok'
+getgenv().UiSize = 1
+loadstring(game:HttpGet('https://xk5ng.github.io/XK5NG'))()
