0b4d2fb78a2a66b34060cf838b293f175b19995b
xanaaaxburns <alphxx.jd@gmail.com>
2025-10-27 00:41:18 +0000

    Create Semi-Legit Loader.lua

---
+++ Semi-Legit Loader.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xanaaaxburns/billieeilish/refs/heads/main/fpsWM.lua'))()
+task.wait(2)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xanaaaxburns/billieeilish/refs/heads/main/DHLock.lua'))()
